ed46f4453a268db64da9a40515cd0888bdcd384f
Christoph Herrmann <developer@dragonprojects.de>
2016-07-30 18:48:21 +0200

    Tower control attack closest enemy

--- develop/main.js
+++ develop/main.js
@@ -3,9 +3,16 @@
 var memory = require('./memory');
 var spawnController = require('./spawn.controller');
 var creepController = require('./creep.controller');
+var towerController = require('./tower.controller');
 
 module.exports.loop = () => {
   memory();
   _.each(Game.spawns, spawnController);
   _.each(Game.creeps, creepController);
+  _.each(Game.rooms, room => {
+    var towers = room.find(FIND_STRUCTURES, {
+      filter: structure => structure.structureType == STRUCTURE_TOWER
+    });
+    _.each(towers, towerController);
+  });
 };
