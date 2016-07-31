4368a9d2d5f9156d3c2d8be58f7377719a25bba5
Christoph Herrmann <developer@dragonprojects.de>
2016-07-31 02:28:59 +0200

    Let the tower repair structures if have enough energy

--- develop/tower.controller.js
+++ develop/tower.controller.js
@@ -4,5 +4,14 @@ module.exports = tower => {
   var target = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
   if (target) {
     tower.attack(target);
+  } else {
+    if (tower.energy / tower.energyCapacity > 0.5) {
+      var structure = tower.pos.findClosestByPath(FIND_STRUCTURES, {
+        filter: structure => structure.hits < structure.hitsMax && structure.structureType !== STRUCTURE_WALL
+      });
+      if (structure) {
+        tower.repair(structure);
+      }    
+    }
   }
 };
