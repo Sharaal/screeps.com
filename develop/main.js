0a86b7d2dd498bbbb99d75e6c17675cf27a82f9b
Christoph Herrmann <developer@dragonprojects.de>
2016-07-31 02:49:33 +0200

    Memory and spawn controller not needed every tick

--- develop/main.js
+++ develop/main.js
@@ -4,10 +4,15 @@ var memory = require('./memory');
 var spawnController = require('./spawn.controller');
 var creepController = require('./creep.controller');
 var towerController = require('./tower.controller');
+var time = require('./util.time');
 
 module.exports.loop = () => {
-  memory();
-  _.each(Game.spawns, spawnController);
+  if (time(100)) {
+    memory();
+  }
+  if (time(10)) {
+    _.each(Game.spawns, spawnController);
+  }
   _.each(Game.creeps, creepController);
   _.each(Game.rooms, room => {
     var towers = room.find(FIND_STRUCTURES, {
