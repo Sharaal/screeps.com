fcdd88a33a179bed82ef68e17721440cb2acaeee
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-08-01 15:03:39 +0200

    Renew creep if he needs it and is currently near a spawn

---
+++ develop/creep.activity.renew.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function needRenew(creep) {
+  var missing = 1500 - creep.ticksToLive;
+  var renew = Math.floor(600 / creep.body.length);
+  return missing >= renew;
+}
+
+module.exports = (creep, structureSpawn) => {
+  if (creep.body.indexOf(CLAIM) !== -1) {
+    return true;
+  }
+  if (!needRenew(creep)) {
+    return true;
+  }
+  structureSpawn.renewCreep(creep);
+};
--- develop/creep.activity.transferStructure.js
+++ develop/creep.activity.transferStructure.js
@@ -1,7 +1,12 @@
 'use strict';
 
+var renew = require('./creep.activity.renew');
+
 module.exports = creep => {
   var transferStructure = Game.getObjectById(creep.memory.transferStructure);
   creep.transfer(transferStructure, RESOURCE_ENERGY);
+  if (transferStructure.structureType === STRUCTURE_SPAWN) {
+    renew(creep, transferStructure);
+  }
   return true;
 };
