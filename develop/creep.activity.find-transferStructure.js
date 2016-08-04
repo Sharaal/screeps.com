2ccd8b3a25c3a50f4d2bfbdc2c305f66fb49a24e
Christoph Herrmann <developer@dragonprojects.de>
2016-08-04 21:48:41 +0200

    Remove unused function

--- develop/creep.activity.find-transferStructure.js
+++ develop/creep.activity.find-transferStructure.js
@@ -22,15 +22,6 @@ function findTower(creep) {
   });
 }
 
-function findStorage(creep) {
-  return creep.pos.findClosestByPath(FIND_STRUCTURES, {
-    filter: structure =>
-      structure.structureType == STRUCTURE_STORAGE
-      &&
-      (structure.store.energy + structure.store.L) < structure.storeCapacity
-  });
-}
-
 module.exports = (creep, results) => {
   var transferStructure = findExtensionOrSpawn(creep) || findTower(creep);
   if (transferStructure) {
