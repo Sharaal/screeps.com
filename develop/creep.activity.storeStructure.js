521554994829e98d1fceafccce02cd7e5d9fe226
Christoph Herrmann <developer@dragonprojects.de>
2016-08-04 21:48:51 +0200

    Fix variable name

--- develop/creep.activity.storeStructure.js
+++ develop/creep.activity.storeStructure.js
@@ -3,7 +3,7 @@
 module.exports = (creep, results) => {
   var storeStructure = Game.getObjectById(creep.memory.storeStructure);
   if (creep.transfer(storeStructure, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-    if ((structure.store.energy + structure.store.L) === storeStructure.storeCapacity) {
+    if ((storeStructure.store.energy + storeStructure.store.L) === storeStructure.storeCapacity) {
       delete creep.memory.storeStructure;
       return results.FINISHED;
     }
