c879586251356a0485bbd4dfd6c8fc3173107cab
Christoph Herrmann <developer@dragonprojects.de>
2016-08-04 20:51:09 +0200

    If the structure is not in range but full of energy another creep transfered and its not needed to still walk to it

--- develop/creep.activity.transferStructure.js
+++ develop/creep.activity.transferStructure.js
@@ -3,6 +3,10 @@
 module.exports = (creep, results) => {
   var transferStructure = Game.getObjectById(creep.memory.transferStructure);
   if (creep.transfer(transferStructure, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+    if (transferStructure.energy === transferStructure.energyCapacity) {
+      delete creep.memory.transferStructure;
+      return results.FINISHED;
+    }
     creep.moveTo(transferStructure);
     return results.NEXTTICK;
   }
