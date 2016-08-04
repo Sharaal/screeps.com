8ba21c11be0608d0380e8c523274b77e9cd8a1e8
Christoph Herrmann <developer@dragonprojects.de>
2016-08-04 21:40:44 +0200

    Transfer first extension and spawn, then tower

--- develop/creep.activity.find-transferStructure.js
+++ develop/creep.activity.find-transferStructure.js
@@ -1,18 +1,38 @@
 'use strict';
 
-module.exports = (creep, results) => {
-  var transferStructure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+function findExtensionOrSpawn(creep) {
+  return creep.pos.findClosestByPath(FIND_STRUCTURES, {
     filter: structure =>
       (
         structure.structureType == STRUCTURE_EXTENSION
         ||
         structure.structureType == STRUCTURE_SPAWN
-        ||
-        structure.structureType == STRUCTURE_TOWER
       )
       &&
       structure.energy < structure.energyCapacity
   });
+}
+
+function findTower(creep) {
+  return creep.pos.findClosestByPath(FIND_STRUCTURES, {
+    filter: structure =>
+      structure.structureType == STRUCTURE_TOWER
+      &&
+      structure.energy < structure.energyCapacity
+  });
+}
+
+function findStorage(creep) {
+  return creep.pos.findClosestByPath(FIND_STRUCTURES, {
+    filter: structure =>
+      structure.structureType == STRUCTURE_STORAGE
+      &&
+      (structure.store.energy + structure.store.L) < structure.storeCapacity
+  });
+}
+
+module.exports = (creep, results) => {
+  var transferStructure = findExtensionOrSpawn(creep) || findTower(creep);
   if (transferStructure) {
     creep.memory.transferStructure = transferStructure.id;
   }
