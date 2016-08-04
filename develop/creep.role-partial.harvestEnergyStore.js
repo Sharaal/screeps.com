b3254a867b1c4ce28edb5dacf3ab18a3225b28b7
Christoph Herrmann <developer@dragonprojects.de>
2016-08-05 01:27:40 +0200

    Add missing semicolons

--- develop/creep.role-partial.harvestEnergyStore.js
+++ develop/creep.role-partial.harvestEnergyStore.js
@@ -20,5 +20,5 @@ module.exports = (next, fallback) => {
         return fallback;
       }
     },
-  }
+  };
 };
--- develop/creep.role-partial.harvestSource.js
+++ develop/creep.role-partial.harvestSource.js
@@ -15,5 +15,5 @@ module.exports = (next, fallback) => {
         return fallback;
       }
     },
-  }
+  };
 };
