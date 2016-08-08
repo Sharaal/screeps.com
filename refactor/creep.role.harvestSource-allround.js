3fe1f7963f952b715e2deffe702162a17cdb4074
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-08-08 15:37:56 +0200

    Formatting

--- refactor/creep.role.harvestSource-allround.js
+++ refactor/creep.role.harvestSource-allround.js
@@ -6,10 +6,10 @@ module.exports = {
     startActivity: harvest,
     activities: _.merge(
       {},
-      require('./creep.activity.harvestSource')('transferStructure', harvest),
-      require('./creep.activity.transferStructure')('buildConstructionSite', harvest),
-      require('./creep.activity.buildConstructionSite')('upgradeController', harvest),
-      require('./creep.activity.upgradeController')('upgradeController', harvest)
+      require('./creep.activity.harvestSource')        ('transferStructure',     harvest),
+      require('./creep.activity.transferStructure')    ('buildConstructionSite', harvest),
+      require('./creep.activity.buildConstructionSite')('upgradeController',     harvest),
+      require('./creep.activity.upgradeController')    ('upgradeController',     harvest)
     )
   }
 };
