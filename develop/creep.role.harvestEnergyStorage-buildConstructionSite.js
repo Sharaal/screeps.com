6ba14cc5bdf7b018482115f47a11c3b926141aa3
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-08-09 11:15:39 +0200

    Use the rest of the lifetime to upgrade the controller

--- develop/creep.role.harvestEnergyStorage-buildConstructionSite.js
+++ develop/creep.role.harvestEnergyStorage-buildConstructionSite.js
@@ -7,7 +7,8 @@ module.exports = {
     activities: _.merge(
       {},
       require('./creep.activity.harvestEnergyStorage') ('buildConstructionSite', harvest),
-      require('./creep.activity.buildConstructionSite')('buildConstructionSite', harvest)
+      require('./creep.activity.buildConstructionSite')('upgradeController', harvest),
+      require('./creep.activity.upgradeController')    ('upgradeController', harvest)
     ),
     roomConditions: room => room.find(FIND_CONSTRUCTION_SITES).length > 0
   }
