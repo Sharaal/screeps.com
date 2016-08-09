eb1a8ccc84d334f82c9f995908e6a539ffb5f6b4
Christoph Herrmann <developer@dragonprojects.de>
2016-08-09 22:21:43 +0200

    activity harvestDroppedEnergy and role carrier

--- develop/creep.role.carrier.js
+++ develop/creep.role.carrier.js
@@ -2,12 +2,14 @@
 
 var harvest = 'harvestDroppedEnergy';
 module.exports = {
+  'carrier': {
     startActivity: harvest,
     activities: _.merge(
       {},
       require('./creep.activity.harvestDroppedEnergy') ('transferEnergyStorage', 'harvestEnergyStorage'),
       require('./creep.activity.transferEnergyStorage')('transferStructure',     harvest),
       require('./creep.activity.harvestEnergyStorage') ('transferStructure',     harvest),
+      require('./creep.activity.transferStructure')    ('harvestDroppedEnergy',  harvest)
     )
   }
 };
