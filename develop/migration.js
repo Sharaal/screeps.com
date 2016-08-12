fd86f5bfc5ea6947fe79a109470535944a9a80e2
Christoph Herrmann <developer@dragonprojects.de>
2016-08-12 19:08:29 +0200

    Also use migration from level 0 to 1 to init the new scripts

--- develop/migration.js
+++ develop/migration.js
@@ -3,14 +3,11 @@
 var currentVersion = 1;
 
 module.exports = roles => {
-  if (!Memory.version) {
-    Memory.version = currentVersion;
-    return;
-  }
+  Memory.version = Memory.version || 0;
   if (Memory.version === currentVersion) {
     return;
   }
   for (; Memory.version < currentVersion; ++Memory.version) {
-    require(`./migration.${Memory.version}`)(roles);
+    require(`./migration.version.${Memory.version}`)(roles);
   }
 };
---
+++ develop/migration.version.0.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var changeRoles = require('./util.migration.changeRoles');
+
+module.exports = roles => {
+  changeRoles(roles, 'carrier',                                    'storageCarrier');
+  changeRoles(roles, 'harvestEnergyStorage-buildConstructionSite', 'storageBuilder');
+  changeRoles(roles, 'harvestEnergyStorage-buildSpawn',            'storageSpawnBuilder');
+  changeRoles(roles, 'harvestEnergyStorage-transferStructure',     'storageCarrier');
+  changeRoles(roles, 'harvestEnergyStorage-upgradeController',     'storageUpgrader');
+  changeRoles(roles, 'harvestSource-allround',                     'sourceAllrounder');
+  changeRoles(roles, 'harvestSource-transferEnergyStorage',        'storageSourcer');
+  changeRoles(roles, 'harvestSource-upgradeController',            'sourceUpgrader');
+};
