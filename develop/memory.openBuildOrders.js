25f29e4513447f64efb7712b3e262d50e0ee5d44
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-08-17 17:19:08 +0200

    Store open build orders in memory to check if all structures which are needed are built

---
+++ develop/memory.openBuildOrders.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function init() {
+  if (!Memory.openBuildOrders) {
+    Memory.openBuildOrders = {};
+  }
+}
+
+module.exports.garbageCollector = () => {
+  init();
+  for(var roomName in Memory.openBuildOrders) {
+    if(!Game.rooms[roomName]) {
+      delete Memory.openBuildOrders[roomName];
+    }
+  }
+};
+
+module.exports.set = (roomName, buildOrders) => {
+  init();
+  Memory.openBuildOrders[roomName] = buildOrders;
+};
+
+module.exports.finished = (roomName) => {
+  init();
+  return Memory.openBuildOrders[roomName] && Memory.openBuildOrders[roomName].length === 0;
+};
