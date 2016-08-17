61531baf39f6c071404985c524390a6759a6bc44
Christoph Herrmann <developer@dragonprojects.de>
2016-08-17 20:24:51 +0200

    Rescue always at 2500 ticks

--- develop/creep.role.worker.js
+++ develop/creep.role.worker.js
@@ -7,7 +7,7 @@ module.exports = {
     'withdrawEnergyStorage':   require('./creep.activity.withdrawEnergyStorage')  ('rescueController',      'withdrawEnergyContainer'),
     'withdrawEnergyContainer': require('./creep.activity.withdrawEnergyContainer')('rescueController',      'pickupDroppedEnergy'),
     'pickupDroppedEnergy':     require('./creep.activity.pickupDroppedEnergy')    ('rescueController',      empty),
-    'rescueController':        require('./creep.activity.upgradeController')      ('buildConstructionSite', empty, { ticksToDowngrade: 3500 }),
+    'rescueController':        require('./creep.activity.upgradeController')      ('buildConstructionSite', empty, { ticksToDowngrade: 2500 }),
     'buildConstructionSite':   require('./creep.activity.buildConstructionSite')  ('upgradeController',     empty),
     'upgradeController':       require('./creep.activity.upgradeController')      ('upgradeController',     empty)
   }
