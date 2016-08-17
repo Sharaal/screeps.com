b94b193c3539489b5588512610f18481f4471d5a
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-08-17 12:31:32 +0200

    Formatting

--- develop/creep.role.rescuer.js
+++ develop/creep.role.rescuer.js
@@ -4,10 +4,10 @@ var harvest = 'harvestEnergyStorage';
 module.exports = {
   startActivity: harvest,
   activities: {
-    'harvestEnergyStorage':  require('./creep.activity.harvestEnergyStorage')('rescueController', 'harvestSource'),
-    'harvestSource':         require('./creep.activity.harvestSource')       ('rescueController',  harvest),
-    'rescueController':      require('./creep.activity.upgradeController')   ('transferStructure', harvest, { ticksToDowngrade: 3500 }),
-    'transferStructure':     require('./creep.activity.transferStructure')   ('upgradeController', harvest),
-    'upgradeController':     require('./creep.activity.upgradeController')   ('upgradeController', harvest)
+    'harvestEnergyStorage': require('./creep.activity.harvestEnergyStorage')('rescueController', 'harvestSource'),
+    'harvestSource':        require('./creep.activity.harvestSource')       ('rescueController',  harvest),
+    'rescueController':     require('./creep.activity.upgradeController')   ('transferStructure', harvest, { ticksToDowngrade: 3500 }),
+    'transferStructure':    require('./creep.activity.transferStructure')   ('upgradeController', harvest),
+    'upgradeController':    require('./creep.activity.upgradeController')   ('upgradeController', harvest)
   }
 };
