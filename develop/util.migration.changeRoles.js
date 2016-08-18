80d82d3b775f873923386949e9808fb82c6436c5
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-08-18 17:36:28 +0200

    Fix use first activity as startActivity

--- develop/util.migration.changeRoles.js
+++ develop/util.migration.changeRoles.js
@@ -2,13 +2,14 @@
 
 module.exports = (roles, oldRole, newRole) => {
   _.each(Game.creeps, creep => {
-    if (creep.memory.role === oldRole) {
-      creep.memory.role = newRole;
-      const role = roles[newRole];
-      if (!role) {
-        return;
-      }
-      creep.memory.activity = roles[newRole].startActivity;
+    if (creep.memory.role !== oldRole) {
+      return;
     }
+    creep.memory.role = newRole;
+    const role = roles[newRole];
+    if (!role) {
+      return;
+    }
+    creep.memory.activity = Object.keys(role.activities)[0];
   });
 };
