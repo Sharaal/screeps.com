9af1885f0bde3a7cf0d2c590f8af165102532947
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-08-18 17:52:21 +0200

    Rename

--- develop/migration.js
+++ develop/migration.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const currentVersion = 3;
+const VERSION = 3;
 
 module.exports = roles => {
   Memory.version = Memory.version || 0;
-  if (Memory.version === currentVersion) {
+  if (Memory.version === VERSION) {
     return;
   }
-  for (; Memory.version < currentVersion; ++Memory.version) {
+  for (; Memory.version < VERSION; ++Memory.version) {
     require(`./migration.version.${Memory.version}`)(roles);
   }
 };
