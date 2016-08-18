7ab8fe6fa848bfa3ba3d014710d13ee11b6df0ba
Christoph Herrmann <developer@dragonprojects.de>
2016-08-19 00:52:57 +0200

    Add flagTanker which can move to and tank a flag

--- develop/creep.activity.moveToFlagRoom.js
+++ develop/creep.activity.moveToFlagRoom.js
@@ -3,8 +3,10 @@
 module.exports = (next, opts) => creep => {
   const flag = Game.flags[opts.flagName];
   if (!flag) {
+    creep.error('missing flag', { flagName: opts.flagName });
     return;
   }
+  if (!flag.room || flag.room.name !== creep.room.name) {
     creep.moveTo(flag);
     return;
   }
