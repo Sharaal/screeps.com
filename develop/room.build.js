8102e069d7f0c058d6f42c38eab1ccefb89c9acc
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-08-18 12:33:11 +0200

    Use game data to determine the needed structures

--- develop/room.build.js
+++ develop/room.build.js
@@ -1,54 +1,64 @@
 'use strict';
 
 var openBuildOrders = require('./memory.openBuildOrders');
-var levels = [
-  require('./room.build.level.1'),
-  require('./room.build.level.2'),
-  require('./room.build.level.3'),
-  require('./room.build.level.4'),
-  require('./room.build.level.5'),
-  require('./room.build.level.6'),
-  require('./room.build.level.7'),
-  require('./room.build.level.8')
-];
+
+function getNeededAmount(room, structureType, maxNeededAmount) {
+  var controllerMaxAmount = CONTROLLER_STRUCTURES[structureType][room.controller.level];
+  if (maxNeededAmount) {
+    Math.min(controllerMaxAmount, maxNeededAmount);
+  }
+  return controllerMaxAmount;
+}
+
+function getNeededStructures(room) {
+  var neededStructures = {};
+  neededStructures[STRUCTURE_SPAWN]     = getNeededAmount(room, STRUCTURE_SPAWN);
+  neededStructures[STRUCTURE_EXTENSION] = getNeededAmount(room, STRUCTURE_EXTENSION);
+  neededStructures[STRUCTURE_TOWER]     = getNeededAmount(room, STRUCTURE_TOWER);
+  neededStructures[STRUCTURE_STORAGE]   = getNeededAmount(room, STRUCTURE_STORAGE, room.find(FIND_SOURCES).length);
+  neededStructures[STRUCTURE_CONTAINER] = getNeededAmount(room, STRUCTURE_CONTAINER, room.find(FIND_SOURCES).length - structures[STRUCTURE_STORAGE]);
+  return neededStructures;
+}
+
+function filterAvailableStructures(room, neededStructures) {
+  var filteredNeededStructures = {};
+  _.each(neededStructures, (neededAmount, neededStructureType) => {
+    var availableStructures = room.find(FIND_STRUCTURES, {
+      filter: structure => structure.structureType === neededStructureType
+    });
+    neededAmount = neededAmount - availableStructures.length;
+    if (neededAmount > 0) {
+      filteredNeededStructures[neededStructureType] = neededAmount;
+    }
+  });
+  return filteredNeededStructures;
+}
+
+function filterAvailableConstructionSites(room, neededStructures) {
+  var filteredNeededStructures = {};
+  _.each(neededStructures, (neededAmount, neededStructureType) => {
+    var availableConstructionSites = room.find(FIND_MY_CONSTRUCTION_SITES, {
+      filter: constructionSite => constructionSite.structureType === neededStructureType
+    });
+    neededAmount = neededAmount - availableConstructionSites.length;
+    if (neededAmount > 0) {
+      filteredNeededStructures[neededStructureType] = neededAmount;
+    }
+  });
+  return filteredNeededStructures;
+}
 
 module.exports = room => {
-  var neededStructures;
-  _.each(levels, level => {
-    if (!level.conditions(room)) {
-      return;
-    }
-    neededStructures = level.structures(room);
-  });
-  if (!neededStructures) {
-    return;
-  }
+  var neededStructures = getNeededStructures(room);
 
-  var newBuildOrders = [];
-  var buildOrders = [];
-  _.each(neededStructures, neededStructure => {
-    var availableStructures = room.find(FIND_STRUCTURES, {
-      filter: structure => structure.structureType === neededStructure.structureType
-    });
-    var amount = neededStructure.amount - availableStructures.length;
-    if (amount > 0) {
-      buildOrders.push({ structureType: neededStructure.structureType, amount: amount });
-    }
-    var availableConstructionSites = room.find(FIND_CONSTRUCTION_SITES, {
-      filter: constructionSite => constructionSite.structureType === neededStructure.structureType
-    });
-    var newAmount = neededStructure.amount - (availableStructures.length + availableConstructionSites.length);
-    if (newAmount > 0) {
-      newBuildOrders.push({ structureType: neededStructure.structureType, amount: newAmount });
-    }
-  });
+  neededStructures = filterAvailableStructures(room, neededStructures);
+  openBuildOrders.set(room.name, neededStructures);
 
-  if (newBuildOrders.length) {
-    console.log('------------------------------ ROOM NEW BUILD ORDERS ------------------------------');
+  neededStructures = filterAvailableConstructionSites(room, neededStructures);
+  if (Object.keys(neededStructures).length) {
+    console.log('------------------------------ ROOM NEEDED CONSTRUCTION SITES ------------------------------');
     console.log(room.name);
-    console.log(JSON.stringify(newBuildOrders));
-    console.log('-------------------------------------------------------------------------------');
+    console.log(JSON.stringify(neededStructures));
+    console.log('--------------------------------------------------------------------------------------------');
   }
-
-  openBuildOrders.set(room.name, buildOrders);
 };
