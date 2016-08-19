77867447576a0797f8434929465808efd8fbdd71
Christoph Herrmann <developer@dragonprojects.de>
2016-08-19 02:37:57 +0200

    flagAttacker move to the attack target flag and attack all hostile towers, creeps, spawns and structures

---
+++ develop/creep.role.flagAttacker.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const spawnFlagName = 'attack spawn';
+const targetFlagName = 'attack target';
+
+module.exports = {
+  activities: {
+    'moveToFlag':    require('./creep.activity.moveToFlag')   ('attackHostile', { flagName: targetFlagName }),
+    'attackHostile': require('./creep.activity.attackHostile')('moveToFlag')
+  },
+  spawn: room => {
+    const flag = Game.flags[spawnFlagName];
+    if (!flag) {
+      return;
+    }
+    if (!flag.room || room.name !== flag.room.name) {
+      return;
+    }
+    return {
+      body: { attack: 5, move: 5 }
+    };
+  }
+};
--- develop/creep.roles.js
+++ develop/creep.roles.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   'carrier':      require('./creep.role.carrier'),
+  'flagAttacker': require('./creep.role.flagAttacker'),
   'flagClaimer':  require('./creep.role.flagClaimer'),
   'flagTanker':   require('./creep.role.flagTanker'),
   'rescuer':      require('./creep.role.rescuer'),
