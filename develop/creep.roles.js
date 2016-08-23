277acfbe693e2d970e256aa9f73f54a03164381d
Christoph Herrmann <developer@dragonprojects.de>
2016-08-23 18:56:43 +0200

    Add the new "neighboringReserver" role

--- develop/creep.roles.js
+++ develop/creep.roles.js
@@ -1,12 +1,13 @@
 'use strict';
 
 module.exports = {
-  'carrier':      require('./creep.role.carrier'),
-  'flagAttacker': require('./creep.role.flagAttacker'),
-  'flagClaimer':  require('./creep.role.flagClaimer'),
+  'carrier':             require('./creep.role.carrier'),
+  'flagAttacker':        require('./creep.role.flagAttacker'),
+  'flagClaimer':         require('./creep.role.flagClaimer'),
+  'neighboringReserver': require('./creep.role.neighboringSourcer'),
   'neighboringSourcer':  require('./creep.role.neighboringSourcer'),
-  'rescuer':      require('./creep.role.rescuer'),
-  'sourcer':      require('./creep.role.sourcer'),
-  'spawnBuilder': require('./creep.role.spawnBuilder'),
-  'worker':       require('./creep.role.worker')
+  'rescuer':             require('./creep.role.rescuer'),
+  'sourcer':             require('./creep.role.sourcer'),
+  'spawnBuilder':        require('./creep.role.spawnBuilder'),
+  'worker':              require('./creep.role.worker')
 };
