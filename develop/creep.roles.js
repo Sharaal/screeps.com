e9985ef862915e189b1e5945cf75cf5bdc416628
Christoph Herrmann <developer@dragonprojects.de>
2016-08-23 19:18:18 +0200

    Fix roles definition

--- develop/creep.roles.js
+++ develop/creep.roles.js
@@ -4,7 +4,7 @@ module.exports = {
   'carrier':             require('./creep.role.carrier'),
   'flagAttacker':        require('./creep.role.flagAttacker'),
   'flagClaimer':         require('./creep.role.flagClaimer'),
-  'neighboringReserver': require('./creep.role.neighboringSourcer'),
+  'neighboringReserver': require('./creep.role.neighboringReserver'),
   'neighboringSourcer':  require('./creep.role.neighboringSourcer'),
   'rescuer':             require('./creep.role.rescuer'),
   'sourcer':             require('./creep.role.sourcer'),
